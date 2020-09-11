309a0f558ecd4dcf84cfdf8b4441a2272059afea
Derek Kershner <25798427+dkershner6@users.noreply.github.com>
2020-09-10 19:20:47 -0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    preset: 'ts-jest',
+    roots: ['<rootDir>/src'],
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    testPathIgnorePatterns: ['./node_modules/', './demo-site', 'dist'],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+    coveragePathIgnorePatterns: ['node_modules', 'src/index.ts'],
+    globals: {
+        // we must specify a custom tsconfig for tests because we need the typescript transform
+        // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+        // can see this setting in tsconfig.jest.json -> "jsx": "react"
+        'ts-jest': {
+            tsConfig: 'tsconfig.test.json'
+        }
+    }
+};
